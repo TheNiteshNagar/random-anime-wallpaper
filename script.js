b4d42526d6297bcd53eec3bce551a6f952c43f85
Nitesh Nagar <159615163+xitesh@users.noreply.github.com>
2026-01-25 21:14:15 +0530

    Refactor getAnime function and add getImagesCategories for improved category selection

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-async function getAnime(){
-  const randoCategory = getImagesCategories()
-  const data = await fetch(`https://nekos.best/api/v2/${randoCategory}`)
-  const response = await data.json()
-  return response.results[0].url
-}
-
-function getImagesCategories(){
-  const randomIndex = Math.floor(Math.random()*4)
+// generate a random category
+function getImagesCategories() {
+  const randomIndex = Math.floor(Math.random() * 4)
   const images = ['husbando', 'kitsune', 'neko', 'waifu']
   return images[randomIndex]
 }
 
-document.querySelector('body').addEventListener('click', async ()=>{
+// get a anime url
+async function getAnime() {
+  const randoCategory = getImagesCategories()
+  const data = await fetch(`https://nekos.best/api/v2/${randoCategory}`)
+  const response = await data.json()
+  const animeURL = response.results[0].url
+  return animeURL
+}
+
+// show anime image when dom content loaded
+window.addEventListener('DOMContentLoaded', async () => {
   const animeURL = await getAnime()
   document.querySelector('.anime-pic').style.backgroundImage = `url(${animeURL})`
 })
 
-
-window.addEventListener('DOMContentLoaded', async () =>{
+// get a new anime image user click on body
+document.querySelector('body').addEventListener('click', async () => {
   const animeURL = await getAnime()
-  document.querySelector('.anime-pic').style.backgroundImage = `url(${animeURL})`
+  document.querySelector('.anime-pic').style.backgroundImage = `url(${animeURL})`    
 })
